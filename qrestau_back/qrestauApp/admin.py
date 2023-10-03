a8bf6bc15d3fc76c89409104f7767e77b8e3857a
Thomas Figved <thomas.figved@gmail.com>
2023-10-03 15:17:48 +0900

    Adding models to admin screen

--- qrestau_back/qrestauApp/admin.py
+++ qrestau_back/qrestauApp/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Item, Category, Table, Meal, MealItem
 
 # Register your models here.
+admin.site.register(Item)
+admin.site.register(Category)
+admin.site.register(Table)
+admin.site.register(Meal)
+admin.site.register(MealItem)
