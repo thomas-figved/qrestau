108e8a33099209e2952ee55610f8e881f12ad1b7
Thomas Figved <thomas.figved@gmail.com>
2023-10-24 10:41:26 +0900

    Adding allowed_hosts

--- qrestau_back/qrestau/settings.py
+++ qrestau_back/qrestau/settings.py
@@ -155,4 +155,7 @@ DJOSER = {
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:3000',
     'http://127.0.0.1:3000',
+    'ljpepka.cluster030.hosting.ovh.net',
 ]
+
+ALLOWED_HOSTS = ['ljpepka.cluster030.hosting.ovh.net', '127.0.0.1', 'localhost']
